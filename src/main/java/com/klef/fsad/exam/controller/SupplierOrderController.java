f338e37fd529c9a6f638ea506b74db1f5739ac01
phanidurgalikhitha <2400030747@kluniversity.in>
2026-05-02 20:36:39 +0530

    second commit

--- src/main/java/com/klef/fsad/exam/controller/SupplierOrderController.java
+++ src/main/java/com/klef/fsad/exam/controller/SupplierOrderController.java
@@ -1,5 +1,28 @@
 package com.klef.fsad.exam.controller;
 
-public class SupplierOrderController {
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+import com.klef.fsad.exam.model.SupplierOrder;
+import com.klef.fsad.exam.service.SupplierOrderService;
+
+@RestController
+@RequestMapping("/supplier")
+public class SupplierOrderController 
+{
+    @Autowired
+    private SupplierOrderService service;
+
+    @PostMapping("/add")
+    public SupplierOrder addOrder(@RequestBody SupplierOrder order)
+    {
+        return service.addOrder(order);
+    }
+
+    @GetMapping("/all")
+    public List<SupplierOrder> getAllOrders()
+    {
+        return service.getAllOrders();
+    }
 }
