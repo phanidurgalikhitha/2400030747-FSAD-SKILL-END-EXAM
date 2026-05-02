18b10b175cfb82d5e53c95adde3c3ae39848020e
phanidurgalikhitha <2400030747@kluniversity.in>
2026-05-02 20:37:23 +0530

    third commit

--- src/main/java/com/klef/fsad/exam/model/SupplierOrder.java
+++ src/main/java/com/klef/fsad/exam/model/SupplierOrder.java
@@ -1,5 +1,27 @@
 package com.klef.fsad.exam.model;
 
-public class SupplierOrder {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
+@Entity
+public class SupplierOrder 
+{
+    @Id   // manual ID (IMPORTANT)
+    private int id;
+
+    private String name;
+    private String date;
+    private String status;
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getDate() { return date; }
+    public void setDate(String date) { this.date = date; }
+
+    public String getStatus() { return status; }
+    public void setStatus(String status) { this.status = status; }
 }
